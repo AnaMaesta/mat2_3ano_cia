b07db5b98e2dbc513a4d0aea2f7822c054fd721f
✩ maesta__:P <114663705+AnaMaesta@users.noreply.github.com>
2024-05-23 17:19:32 -0300

    'main'

--- main.js
+++ main.js
@@ -14,4 +14,5 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 const contador = document.querySelectorAll(".contador");
-contadores[0].textContent = "Contagem Regressiva";
+
+contadores[0].textContent = "Contagem regressiva";
